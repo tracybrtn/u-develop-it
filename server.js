78a5122862993a03381289a1de0227eaca110823
tracybrtn <tracynburton@gmail.com>
2022-09-30 09:18:43 -0500

    commented requested server queries

--- server.js
+++ server.js
@@ -21,14 +21,42 @@ const db = mysql.createConnection(
     console.log('Connected to the election database.')
   );
 
-db.query(`SELECT * FROM candidates`, (err, rows) => {
-console.log(rows);
-});
+// db.query(`SELECT * FROM candidates`, (err, rows) => {
+// console.log(rows);
+// });
 
-// Default response for any other request (Not Found)
-app.use((req, res) => {
-    res.status(404).end();
-  });
+// GET a single candidate
+// db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+//     if (err) {
+//       console.log(err);
+//     }
+//     console.log(row);
+//   });
+
+// Delete a candidate
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//     if (err) {
+//       console.log(err);
+//     }
+//     console.log(result);
+//   });
+
+// Create a candidate
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
+//               VALUES (?,?,?,?)`;
+// const params = [1, 'Ronald', 'Firbank', 1];
+
+// db.query(sql, params, (err, result) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(result);
+// });
+
+// // Default response for any other request (Not Found)
+// app.use((req, res) => {
+//     res.status(404).end();
+//   });
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
