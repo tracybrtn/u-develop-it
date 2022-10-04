2d3beef35a4c7ccd1e51c8cf79573ad53df0561a
tracybrtn <tracynburton@gmail.com>
2022-10-04 14:55:14 -0500

    Working route get request

--- Routes/apiRoutes/voteRoutes.js
+++ Routes/apiRoutes/voteRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const db = require('../../db/connection');
 const inputCheck = require('../../utils/inputCheck');
 
-router.post('/vote', ({  body  }, res) => {
+router.post('/vote', ({ body }, res) => {
     // Data validation
     const errors = inputCheck(body, 'voter_id', 'candidate_id');
     if (errors) {
@@ -16,13 +16,33 @@ router.post('/vote', ({  body  }, res) => {
 
     db.query(sql, params, (err, result) => {
         if (err) {
-            res.status(400).json({  error: err.message  });
+            res.status(400).json({ error: err.message });
             return;
         }
         res.json({
             message: 'success',
             data: body,
             changes: result.affectedRows
+        });
+    });
+});
+
+router.get('/votes', (req, res) => {
+    const sql = `SELECT candidates.*, parties.name AS party_name, 
+                COUNT(candidate_id) 
+                AS count FROM votes 
+                LEFT JOIN candidates ON votes.candidate_id = candidates.id 
+                LEFT JOIN parties ON candidates.party_id = parties.id 
+                GROUP BY candidate_id 
+                ORDER BY count DESC`;
+    db.query(sql, (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: rows
         });
     });
 });
